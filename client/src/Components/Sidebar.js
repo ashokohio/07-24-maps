eaa952363870c93aac561a24ee328db66cc0109e
Claire Liang <claire.liang.us@gmail.com>
2021-08-04 16:18:28 -0400

    sidebar scroll

--- client/src/Components/Sidebar.js
+++ client/src/Components/Sidebar.js
@@ -83,114 +83,118 @@ function Sidebar() {
                     <Dropdown.Item eventKey="option-2">{filters[2]}</Dropdown.Item>
                 </DropdownButton>
 
-                {
-                    (distances.distances.length > 0)
-                    && (durations.durations.length > 0)
-                    && ([...markers]
-                        .sort(
-                            function(a, b) {
-                                return parseFloat( distances.distances.find( el => el[0] === a.id )[1], 10 )
-                                - parseFloat( distances.distances.find( el => el[0] === b.id )[1], 10 );
+                <div style={{height: '650px', overflowY: 'scroll'}}>
+                    {
+                        (distances.distances.length > 0)
+                        && (durations.durations.length > 0)
+                        && ([...markers]
+                            .sort(
+                                function(a, b) {
+                                    return parseFloat( distances.distances.find( el => el[0] === a.id )[1], 10 )
+                                    - parseFloat( distances.distances.find( el => el[0] === b.id )[1], 10 );
+                                }
+                            )
+                            .map( item => {
+
+                            if (filter === "option-1" && !favorites.includes(item.id)) {
+                                return;
+                            } else if (filter === "option-2" && item.status === "out of order") {
+                                return;
                             }
-                        )
-                        .map( item => {
+                            // link directs to Google Maps route planner
+                            let mapLink = "https://www.google.com/maps/dir/?api=1&destination="
+                            + item.location.lat + ","
+                            + item.location.lng;
+                            // get distance from distances array
+                            let distance = distances.distances.find(el => el[0] === item.id);
+                            let duration = durations.durations.find(el => el[0] === item.id);
+                            let variant = "success";
+                            if (item.status === "in use") variant = "warning";
+                            else if (item.status === "out of order") variant = "danger";
 
-                        if (filter === "option-1" && !favorites.includes(item.id)) {
-                            return;
-                        } else if (filter === "option-2" && item.status === "out of order") {
-                            return;
-                        }
-                        // link directs to Google Maps route planner
-                        let mapLink = "https://www.google.com/maps/dir/?api=1&destination="
-                        + item.location.lat + ","
-                        + item.location.lng;
-                        // get distance from distances array
-                        let distance = distances.distances.find(el => el[0] === item.id);
-                        let duration = durations.durations.find(el => el[0] === item.id);
-                        let variant = "success";
-                        if (item.status === "in use") variant = "warning";
-                        else if (item.status === "out of order") variant = "danger";
-
-                        // if item is the selected station, return the "selected" info card
-                        if (item.name === selection) {
-                            return (
-                                <Card key={item.id}
-                                style={{width: '25rem', margin: '5px'}}
-                                onClick={() => handleClick(item.name)}
-                                >
-                                    <Card.Body
-                                    bsPrefix="card-body">
-                                        <Card.Title
-                                        bsPrefix="card-title">
-                                            {item.name}
-                                            <Button 
-                                                disabled size="sm"
-                                                style={{margin: '0px 10px 2px 10px', padding: '0px 5px 0px 5px'}}
-                                                variant={variant}>{item.status}
-                                            </Button>
-                                            {
-                                                favorites.includes(item.id) ? 
-                                                    (<IoStar
-                                                        onClick={() => toggleFave(item)}
-                                                        style={{color: '#0275d8', margin: '0px 0px 3px 0px'}} 
-                                                    />) : (<IoStarOutline
-                                                        onClick={() => toggleFave(item)}
-                                                        style={{color: '#0275d8', margin: '0px 0px 3px 0px'}} 
-                                                    />)
-                                            }
-                                        </Card.Title>
-                                        <Card.Subtitle
-                                        bsPrefix="card-subtitle">{distance[1]}{duration[1]}</Card.Subtitle>
-                                        <Card.Text
-                                        bsPrefix="card-text">This is the charging station at {item.name}.</Card.Text>
-                                        <Button
-                                        href={mapLink}
-                                        variant="primary"
-                                        target="_blank"
-                                        >
-                                            Navigate
-                                        </Button>
-                                    </Card.Body>
-                                </Card>
-                            )
-                        } else { // else, return the greyed info card
-                            return (
-                                <Card key={item.id}
-                                bg="secondary"
-                                style={{width: '25rem', margin: '5px'}}
-                                onClick={() => handleClick(item.name)}
-                                >
-                                    <Card.Body
-                                    bsPrefix="card-body">
-                                        <Card.Title
-                                        bsPrefix="card-title">
-                                            {item.name}
+                            // if item is the selected station, return the "selected" info card
+                            if (item.name === selection) {
+                                return (
+                                    <Card key={item.id}
+                                    style={{width: '25rem', margin: '5px'}}
+                                    onClick={() => handleClick(item.name)}
+                                    >
+                                        <Card.Body
+                                        bsPrefix="card-body">
+                                            <Card.Title
+                                            bsPrefix="card-title">
+                                                {item.name}
+                                                <Button 
+                                                    disabled size="sm"
+                                                    style={{margin: '0px 10px 2px 10px', padding: '0px 5px 0px 5px'}}
+                                                    variant={variant}>{item.status}
+                                                </Button>
+                                                {
+                                                    favorites.includes(item.id) ? 
+                                                        (<IoStar
+                                                            onClick={() => toggleFave(item)}
+                                                            style={{color: '#0275d8', margin: '0px 0px 3px 0px'}} 
+                                                        />) : (<IoStarOutline
+                                                            onClick={() => toggleFave(item)}
+                                                            style={{color: '#0275d8', margin: '0px 0px 3px 0px'}} 
+                                                        />)
+                                                }
+                                            </Card.Title>
+                                            <Card.Subtitle
+                                            bsPrefix="card-subtitle">{distance[1]}{duration[1]}</Card.Subtitle>
+                                            <Card.Text
+                                            bsPrefix="card-text">This is the charging station at {item.name}.</Card.Text>
                                             <Button
-                                            style={{margin: '0px 10px 2px 10px', padding: '0px 5px 0px 5px'}}
-                                            disabled
-                                            size="sm"
-                                            variant={variant}
+                                            href={mapLink}
+                                            variant="primary"
+                                            target="_blank"
                                             >
-                                                {item.status}
+                                                Navigate
                                             </Button>
-                                            {
-                                                favorites.includes(item.id) && (
-                                                    <IoStar
-                                                        style={{color: '#0275d8', margin: '0px 0px 3px 0px'}} 
-                                                    />
-                                                ) 
-                                            }
-                                        </Card.Title>
-                                        <Card.Subtitle
-                                        bsPrefix="card-subtitle">{distance[1]}{duration[1]}</Card.Subtitle>
-                                        <Card.Text
-                                        bsPrefix="card-text">This is the charging station at {item.name}.</Card.Text>
-                                    </Card.Body>
-                                </Card>
-                            )
-                        }
-                    }))
-                }
+                                        </Card.Body>
+                                    </Card>
+                                )
+                            } else { // else, return the greyed info card
+                                return (
+                                    <Card key={item.id}
+                                    bg="secondary"
+                                    style={{width: '25rem', margin: '5px'}}
+                                    onClick={() => handleClick(item.name)}
+                                    >
+                                        <Card.Body
+                                        bsPrefix="card-body">
+                                            <Card.Title
+                                            bsPrefix="card-title">
+                                                {item.name}
+                                                <Button
+                                                style={{margin: '0px 10px 2px 10px', padding: '0px 5px 0px 5px'}}
+                                                disabled
+                                                size="sm"
+                                                variant={variant}
+                                                >
+                                                    {item.status}
+                                                </Button>
+                                                {
+                                                    favorites.includes(item.id) && (
+                                                        <IoStar
+                                                            style={{color: '#0275d8', margin: '0px 0px 3px 0px'}} 
+                                                        />
+                                                    ) 
+                                                }
+                                            </Card.Title>
+                                            <Card.Subtitle
+                                            bsPrefix="card-subtitle">{distance[1]}{duration[1]}</Card.Subtitle>
+                                            <Card.Text
+                                            bsPrefix="card-text">This is the charging station at {item.name}.</Card.Text>
+                                        </Card.Body>
+                                    </Card>
+                                )
+                            }
+                        }))
+                    }                    
+                </div>
+
+
                 
             </Container>
         </div>
